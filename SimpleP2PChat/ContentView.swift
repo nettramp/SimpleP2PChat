b97469d9df44374cebe2944d40def83460bf787d
Nettramp <nettramp@yandex.ru>
2025-09-23 20:14:47 +0300

    Initial Commit

---
+++ SimpleP2PChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleP2PChat
+//
+//  Created by Сергей Ерёмин on 23.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleP2PChat/SimpleP2PChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleP2PChatApp.swift
+//  SimpleP2PChat
+//
+//  Created by Сергей Ерёмин on 23.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleP2PChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
